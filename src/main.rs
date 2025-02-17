fe16d65c41c83be1db289ccd12b3f38b6fa4e21a
code_vibe <mrsamoladapo@gmail.com>
2025-02-17 22:12:43 +0100

    Updated Routes

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,9 @@ async fn main() {
     let rotes_hello = Router::new().route("/", get(|| async { "Hello, World!" }));
 
     //region: ----Start Server
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
-    println!("listening on {}\n", addr);
-    //axum::Server::bind(&addr)
+    let addr = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("listening on {}\n", addr.local_addr().unwrap());
+    axum::serve(addr, rotes_hello).await.unwrap();
     //endregion: ----Start Server
 
 }
