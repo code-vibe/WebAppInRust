e6efbd176f05b9d9aa308ebf551b40e9b529123b
code_vibe <mrsamoladapo@gmail.com>
2025-02-17 22:03:26 +0100

    Add routes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+use axum::Router;
+use axum::routing::get;
+
+#[tokio::main]
+async fn main() {
+    let rotes_hello = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    //region: ----Start Server
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("listening on {}\n", addr);
+    //axum::Server::bind(&addr)
+    //endregion: ----Start Server
+
 }
+
