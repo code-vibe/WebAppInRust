f5322e3f0df9550714bf4f459035cf2cd8da60d3
code_vibe <mrsamoladapo@gmail.com>
2025-02-18 21:56:21 +0100

    Add tests

---
+++ tests/quick_devs.rs
@@ -0,0 +1,10 @@
+use std::error::Error;
+use anyhow::Result;
+
+#[tokio::test]
+async fn test_quick_devs()  -> Result<()> {
+    let hc = httpc_test::new_client("http://0.0.0.0:3000");
+    hc?.do_get("/").await?.print().await?;
+    Ok(())
+
+}
