b3abae33498b5b90cb14ddfa5ed040153584e580
code_vibe <mrsamoladapo@gmail.com>
2025-02-19 22:12:33 +0100

    Update test

--- tests/quick_devs.rs
+++ tests/quick_devs.rs
@@ -5,6 +5,7 @@ use anyhow::Result;
 async fn test_quick_devs()  -> Result<()> {
     let hc = httpc_test::new_client("http://0.0.0.0:3000");
     hc?.do_get("/").await?.print().await?;
+    //
     Ok(())
 
 }
